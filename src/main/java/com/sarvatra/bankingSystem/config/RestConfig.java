7773c72c5a6350480287bfeff74ea78c24dc0a07
Hano Varghese <hanojoby@gmail.com>
2026-01-20 11:29:32 +0530

    config update

---
+++ src/main/java/com/sarvatra/bankingSystem/config/RestConfig.java
@@ -0,0 +1,20 @@
+package com.sarvatra.bankingSystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(
+            RepositoryRestConfiguration config,
+            CorsRegistry cors) {
+
+        cors.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
